a1e7460138d290d8952382cabda1e4aacc69a30d
Arinjay Dhar <87296817+Arinji2@users.noreply.github.com>
2023-11-01 02:35:46 +0530

    Update Model Styles

--- app/(models)/deletePlaylistModel.tsx
+++ app/(models)/deletePlaylistModel.tsx
@@ -27,13 +27,16 @@ export default function DeletePlaylistModel({
         "w-full h-[100svh] p-4 z-[10000] fixed top-0 left-0 bg-black bg-opacity-80 backdrop-blur-sm flex flex-col items-center justify-center"
       }
     >
-      <div className="w-[90%] min-h-[400px] flex flex-col items-center justify-center p-4 md:h-[50%] gap-6  xl:w-[70%] aspect-video bg-palette-background border-[3px] border-black shadow-button">
-        <h1 className="text-[60px] text-palette-text font-medium truncate w-full text-center ">
-          Deleting{" "}
-          <span className="md:text-[60px] text-[45px] xl:text-[80px] font-bold ">
+      <div className="w-[90%] min-h-[400px] flex flex-col items-center  justify-center p-4 md:h-[50%] gap-6  xl:w-[70%] aspect-video bg-palette-background border-[3px] border-black shadow-button">
+        <div className="w-full h-fit flex md:flex-row items-center  justify-center gap-4 flex-col">
+          <h1 className="text-[35px] md:text-[60px] text-palette-text font-medium text-center ">
+            Deleting
+          </h1>
+          <h1 className="md:text-[60px] text-[45px] xl:text-[70px] font-bold text-palette-text truncate w-full md:w-auto  text-center">
             {playlistName}
-          </span>
-        </h1>
+          </h1>
+        </div>
+
         <div className="w-full h-fit flex flex-row items-center justify-center flex-wrap gap-5">
           <button
             onClick={async () => {
