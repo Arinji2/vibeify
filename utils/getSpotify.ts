f02a15574625c13fce6dfad6c834f87894eb43b1
Arinji2 <arinjaydhar205@gmail.com>
2024-08-27 20:59:47 +0530

    Add Documentation

--- utils/getSpotify.ts
+++ utils/getSpotify.ts
@@ -1,3 +1,20 @@
+/*
+There is a chance this breaks due to how we authenticate the Vibeify Account. If it breaks follow the following
+
+1. Go to https://accounts.spotify.com/authorize?client_id=CLIENT_ID_HERE&response_type=code&redirect_uri=https%3A%2F%2Fvibeify.arinji.com&scope=user-read-email%20playlist-modify-public%20playlist-modify-private%20playlist-read-private%20playlist-read-collaborative%20user-read-private
+2. Extract the auth code from this, and then use it in the following curl request
+3. curl -X POST \
+  -H "Authorization: Basic {CREDS_CODE}" \
+  -d grant_type=authorization_code \
+  -d code={AUTH_CODE} \
+  -d redirect_uri=https://vibeify.arinji.com \
+  https://accounts.spotify.com/api/token
+
+  CREDS_CODE can be gotten from this code echo -n 'CLIENT_ID_HERE:CLIENT_SECRET_HERE' | base64
+  AUTH_CODE can be gotten from the auth code in the url
+
+*/
+
 import { SpotifyApi } from "@spotify/web-api-ts-sdk";
 
 export default async function getSpotify() {
@@ -49,5 +66,9 @@ async function refreshTokenHandler(
     body: authOptions.body,
     cache: "no-cache",
   });
+
+  if (res.status === 400)
+    throw new Error("Invalid Credentials. Fix Detailed in Comments");
+
   return res.json();
 }
