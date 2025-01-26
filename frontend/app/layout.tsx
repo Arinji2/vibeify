5fb5867cb3d4ed9d4929b9deed03ef78c94a0d4a
Arinji2 <arinjaydhar205@gmail.com>
2025-01-26 23:43:12 +0530

    Update Imports

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,8 @@
-import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "./globals.css";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
